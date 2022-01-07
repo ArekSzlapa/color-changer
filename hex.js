b6512c6601bf4e170de2bf0cc3678ee10b3e00ad
ArekSzlapa <91280448+ArekSzlapa@users.noreply.github.com>
2022-01-07 16:05:31 +0100

    Finished the color flipper

---
+++ hex.js
@@ -0,0 +1,17 @@
+const colors=[0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
+const btn = document.querySelector(".clickMe");
+const display=document.querySelector("#color");
+
+function getRandomNumber(){
+    return Math.floor(Math.random()*colors.length);
+}
+
+btn.addEventListener('click',function(){
+    let hexColor="#";
+    for (i=0; i<6; i++){
+        hexColor +=colors[getRandomNumber()];
+    }
+    display.textContent=hexColor;
+    document.body.style.backgroundColor=hexColor;
+});
+
